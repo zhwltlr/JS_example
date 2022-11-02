88d92c13585e8c8bfd244ed86cb51c0659313602
ji <vyoungv7340naver.com>
2022-11-02 17:36:23 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현 Mission 2 - 로그인 버튼 활성화 기능 구현 Mission 3 - 댓글 기능 구현

--- src/pages/jiyoungpark/Login/Loginjiyoung.js
+++ src/pages/jiyoungpark/Login/Loginjiyoung.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import './Loginjiyoung.scss';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Loginjiyoung() {
-  return <p>Hello world</p>;
+  const navigate = useNavigate();
+  const [idValue, setIdValue] = useState('');
+  const [pwValue, setPwValue] = useState('');
+
+  const saveUserId = e => {
+    setIdValue(e.target.value);
+  };
+  const saveUserPw = e => {
+    setPwValue(e.target.value);
+  };
+  const inputEnter = e => {
+    if (e.code === 'Enter') {
+      navigate('/main');
+    }
+  };
+
+  const validator =
+    idValue && idValue.includes('@') && pwValue && pwValue.length >= 5;
+
+  return (
+    <main className="login">
+      <div className="loginContainer">
+        <div className="inputContainer">
+          <div className="loginLogo">westagram</div>
+          <form>
+            <input
+              onChange={saveUserId}
+              type="text"
+              name="idInput"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+            />
+            <input
+              onChange={saveUserPw}
+              onKeyDown={inputEnter}
+              type="password"
+              name="pwInput"
+              placeholder="비밀번호"
+            />
+            <button
+              onClick={() => {
+                validator
+                  ? navigate('/main')
+                  : alert('아이디와 비밀번호를 확인해주세요');
+              }}
+              className="loginBtn"
+              disabled={!validator ? true : false}
+            >
+              로그인
+            </button>
+          </form>
+        </div>
+        <p>비밀번호를 잊으셨나요?</p>
+      </div>
+    </main>
+  );
 }
 
 export default Loginjiyoung;
